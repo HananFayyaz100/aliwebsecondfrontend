efaef2a9e4a335624b10a20920184eaa8188b98d
HananFayyaz100 <hanan0371fiaz@gmail.com>
2026-09-24 17:38:49 +0500

    change in admin login

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -19,22 +19,17 @@ const AdminLogin = () => {
       setLoading(true);
       setError("");
 
-<<<<<<< HEAD
       const API_URL =
         import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
       const response = await axios.post(
         `${API_URL}/auth/login`,
-=======
-      const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
->>>>>>> 02321d8cdd869fccb7cf3d208a559c05974d44e0
         {
           email,
           password,
         }
       );
-
+      
       const token = response.data.token;
 
       if (!token) {
