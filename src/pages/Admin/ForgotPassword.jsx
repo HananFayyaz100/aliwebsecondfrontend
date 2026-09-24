47aa321270f8ff78f2207ba6ebebc76335f0229b
HananFayyaz100 <hanan0371fiaz@gmail.com>
2026-09-24 17:40:27 +0500

    forget password chaning

--- src/pages/Admin/ForgotPassword.jsx
+++ src/pages/Admin/ForgotPassword.jsx
@@ -18,16 +18,11 @@ const ForgotPassword = () => {
       setMessage("");
       setError("");
 
-<<<<<<< HEAD
       const API_URL =
         import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
       const response = await axios.post(
         `${API_URL}/auth/forgot-password`,
-=======
-      const response = await axios.post(
-        "http://localhost:5000/api/auth/forgot-password",
->>>>>>> 02321d8cdd869fccb7cf3d208a559c05974d44e0
         {
           email,
         }
